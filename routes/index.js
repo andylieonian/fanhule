9b69e25566cf37c63fec7c8d2207545f95b132e9
andylieonian <348559901@qq.com>
2016-01-07 09:45:50 +0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express beginning'});
+});
+
+router.get('/lee', function(req,res,next) {
+  res.render('lee', { title: 'leezt beginning'});
 });
 
 module.exports = router;
